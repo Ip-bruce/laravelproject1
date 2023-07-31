49708e370c3df858fa2dd2ab7d13c361a3eaa820
Felipe Correia <61751940+Ip-bruce@users.noreply.github.com>
2023-07-30 23:17:02 -0300

    Second Price Text in  the main page

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -3,7 +3,12 @@
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4" >
         <Box v-for="listing in listings" :key="listing.id">
         <Link :href="route('listing.show',{listing: listing.id})" >
+         <div class="flex items-center gap-1" >
          <Price :price="listing.price" class="text-2xl font-bold"/>
+            <div class="text-xs text-gray-500" >
+                <Price :price="400"/> pm
+            </div>
+         </div>
          <ListingSpace :listing = "listing" class="text-lg"/>
          <ListingAddress :listing="listing" class="text-lg font-bold "/>
         </Link>
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -12,11 +12,22 @@
                 <ListingSpace :listing = "listing" class="text-lg"/>
                 <ListingAddress :listing="listing" class="text-lg font-bold "/>
             </Box>
-            <Box class="" >
+            <Box>
                 <template #header >
                     Offer
                 </template>
-                Make an offer
+                <div>
+                    <label class="label"> Interest rate ({{interestRate}}) </label>
+                    <input v-model.number="interestRate" type="range" min="0.1" max="30" step="0.1" class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" >
+                </div>
+                <div>
+                    <label class="label"> Duration ({{duration}}) </label>
+                    <input v-model.number="duration" type="range" min="1" max="35" step="1" class="w-full h-4 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" >
+                </div>
+                <div class="text-gray-600 dark:text-gray-300 mt-2" >
+                    <div class="text-gray-400" >Your monthly payment</div>
+                    <price :price="monthlyPayment" class="text-3xl" />
+                </div>
             </Box>
         </div>
     </div>
@@ -27,7 +38,16 @@ import ListingAddress from '@/Components/ListingAddress.vue';
 import Box from '@/Components/UI/Box.vue';
 import ListingSpace from '@/Components/UI/ListingSpace.vue';
 import Price from '@/Components/UI/Price.vue';
-defineProps({
+import {ref} from 'vue'
+
+import { useMonthlyPayment } from '@/Components/useMonthlyPayment';
+
+const interestRate = ref(2.5)
+const  duration = ref(25)
+
+const props = defineProps({
     listing: Object,
 })
+
+const { monthlyPayment } = useMonthlyPayment(props.listing.price, interestRate, duration)
 </script>
