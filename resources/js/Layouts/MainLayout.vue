fbc601efee7e56c09996981ed80bd819ef286172
Felipe Correia <61751940+Ip-bruce@users.noreply.github.com>
2023-08-08 07:56:20 -0300

    SignIn and Logout

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -9,8 +9,17 @@
           <div class="text-xl text-indigo-800 dark:text-indigo-100 font-bold text-center" >
             <Link :href="route('listing.index')">HomeRent</Link>
           </div>
-          <div>
+          <div class="flex items-center gap-4" v-if="user" >
+            <div class="text-sm text-gray-500" >
+              {{ user.name }}
+            </div>
             <Link :href="route('listing.create')" class="btn-primary" >+New Listing</Link>
+            <div>
+              <Link :href="route('logout')" method="delete" as="button">Logout</Link>
+            </div>
+          </div>
+          <div v-else>
+            <Link :href="route('login')" class="btn-primary" > Sign-In </Link>
           </div>
         </nav>
       </div>
@@ -31,5 +40,8 @@ import { Link, usePage } from '@inertiajs/vue3'
  
 const flashSuccess = computed(
   () => usePage().props.flash.success,
+)
+const user = computed(
+  () => usePage().props.user,
 )
 </script>
