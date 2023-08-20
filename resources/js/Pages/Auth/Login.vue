0dcf18cb68ff0bf8e5a3dacf90945cfa1dcf8693
Felipe Correia <61751940+Ip-bruce@users.noreply.github.com>
2023-08-20 14:19:02 -0300

    Hasing password/Login-Register page routing

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -14,12 +14,15 @@
             <div class="mt-4" >
                 <button class="btn-primary w-full" type="submit" >Login</button>
             </div>
+            <div class="mt-2 text-center" >
+                <Link :href="route('user-account.create')" class="text-sm text-gray-500 dark:text-gray-200" >Need an account? click here</Link>
+            </div>
         </div>
     </form>
 </template>
 
 <script setup>
-import { useForm } from '@inertiajs/vue3';
+import { useForm, Link } from '@inertiajs/vue3';
 
 const form = useForm({
     email: null,
